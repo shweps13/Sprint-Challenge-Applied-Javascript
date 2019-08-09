14e61a67a82693ebb30be46d634c6c5cd40239fe
Heorhii Siburov <g.siburov@gmail.com>
2019-08-09 08:39:32 -0700

    header was fixed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,34 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+
+const headerPlace = document.querySelector('.header-container');
+
+headerPlace.appendChild(Header('SMARCH 28, 2019', 'Lambda Times', '98°'))
+
+function Header(date, title, temp) {
+    // Creating new elements here
+    const header = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    // Making a structure here
+
+    header.appendChild(headerDate);
+    header.appendChild(headerTitle);
+    header.appendChild(headerTemp);
+
+    // Applying styles here
+    header.classList.add('header');
+    header.classList.add('header');
+    headerTemp.classList.add('temp');
+
+    // Adding content here
+    headerDate.textContent = `${date}`;
+    headerTitle.textContent = `${title}`;
+    headerTemp.textContent = `${temp}`;
+
+    return header;
+}
